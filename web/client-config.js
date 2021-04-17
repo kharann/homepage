b15c9c2727db8ce59d34755956d507259f4738df
kharann <anhkhav@gmail.com>
2021-04-17 14:52:29 +0200

    Add gatsby frontend boilerplate

---
+++ web/client-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  sanity: {
+    projectId:
+      process.env.GATSBY_SANITY_PROJECT_ID || "<#< sanity.projectId >#>",
+    dataset: process.env.GATSBY_SANITY_DATASET || "<#< sanity.dataset >#>",
+  },
+};
