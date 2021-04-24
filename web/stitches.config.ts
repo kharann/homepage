7dc4dcbe8cf9cf27166db3c0871fdbc056550fe6
kharann <anhkhav@gmail.com>
2021-04-24 20:26:34 +0200

    Add css reset and some default stitches configuration

---
+++ web/stitches.config.ts
@@ -0,0 +1,21 @@
+import { createCss } from '@stitches/react';
+
+export const { styled, css, getCssString, keyframes } = createCss({
+  theme: {
+    fonts: {
+      system: 'Raleway',
+    },
+    colors: {
+      primary: '#8095FF',
+      richblack: '#0F161E',
+    },
+    fontSizes: {
+      1: '13px',
+      2: '15px',
+      3: '17px',
+    },
+  },
+  utils: {
+    full_bleed: (config) => (value) => value && { width: '100%', gridColumn: '1 / 4' },
+  },
+});
