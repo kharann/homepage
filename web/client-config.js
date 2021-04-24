4c2e967bace7aca8d626e9a728da579f49eb91b2
kharann <anhkhav@gmail.com>
2021-04-24 13:20:41 +0200

    remove generated files which is not necesarry

--- web/client-config.js
+++ web/client-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   sanity: {
     projectId:
-      process.env.GATSBY_SANITY_PROJECT_ID || "<#< sanity.projectId >#>",
-    dataset: process.env.GATSBY_SANITY_DATASET || "<#< sanity.dataset >#>",
+      process.env.GATSBY_SANITY_PROJECT_ID || "r4l4xmqo",
+      dataset: process.env.GATSBY_SANITY_DATASET || "production",
   },
 };
--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,13 +1,7 @@
-import Layout from "../components/layout";
 import React from "react";
-import SEO from "../components/seo";
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <p>NO</p>
 );
 
 export default NotFoundPage;
