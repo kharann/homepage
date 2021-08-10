e69ad818663af2af5a7b03f4528f9e193b8ee089
kharann <anhkhav@gmail.com>
2021-08-10 21:00:09 +0200

    svelte

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,13 @@
+const config = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		colors: {
+			richblack: '#0F161E',
+			primary: '#8595D5'
+		}
+	},
+	plugins: []
+};
+
+module.exports = config;
