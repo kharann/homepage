fb5558ae4fefcb171057fd2f0e85ea26fba65bea
kharann <anhkhav@gmail.com>
2021-12-06 16:09:27 +0100

    add darkmode to footer

--- web/src/components/footer.svelte
+++ web/src/components/footer.svelte
@@ -18,23 +18,26 @@
 </script>
 
 <footer class="mt-8 shadow w-full">
-	<div class="footer_container">
+	<div class="border-primary-2 dark:border-gray-5 footer_container">
 		<div class="content">
 			<div class="grid grid-cols-3 w-max gap-5">
 				{#each iconLinks as iconLink}
-					<a class="text-primary hover:text-primary-5 duration-300" href={iconLink.link}
-						>{@html iconLink.icon}</a
+					<a
+						class="text-primary hover:text-primary-5 dark:text-primary-3 dark:hover:text-primary-4 duration-300"
+						href={iconLink.link}>{@html iconLink.icon}</a
 					>
 				{/each}
 			</div>
-			<div class="pt-4 text-center">© 2021-present Anh-Kha Vo. All rights preserved</div>
+			<div class="pt-4 text-center dark:text-white">
+				© 2021-present Anh-Kha Vo. All rights preserved
+			</div>
 		</div>
 	</div>
 </footer>
 
 <style lang="postcss">
 	.footer_container {
-		@apply pt-6 pb-6 border-t-2 border-primary-2 flex justify-center mr-4 ml-4;
+		@apply pt-6 pb-6 border-t-2 flex justify-center mr-4 ml-4;
 	}
 	.content {
 		@apply max-w-4xl w-full m-auto h-full flex flex-col items-center;
