d8432f1db228f68bee12cbcf261b003d3fee2f4b
kharann <anhkhav@gmail.com>
2021-12-06 15:37:37 +0100

    fix index page

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -22,6 +22,7 @@
 	import type { PreviewPost } from '@api/all-post';
 	import feather from 'feather-icons';
 	import PostListItem from '@components/post-list-item.svelte';
+	import Section from '@components/frontpage-section.svelte';
 
 	export let posts: PreviewPost[];
 	export let role: string;
@@ -32,25 +33,21 @@
 
 <section class="mt-4">
 	<h1 class="text-3xl mb-2">
-		Hei, I'm Anh-Kha Vo and on the web I go by <span class="name">Kharann 👋</span>
+		Hei, I'm Anh-Kha Vo and on the web I go by <span
+			class="text-primary dark:text-primary-3 font-black font-hack">Kharann 👋</span
+		>
 	</h1>
-	<byline class="text-lg text-gray dark:text-gray-2">{role}</byline>
+	<byline class="text-lg text-gray dark:text-gray-3">{role}</byline>
 	<p class="mt-4">{description}</p>
 </section>
-<section class="mt-8">
-	<h2 class="text-2xl font-bold text-secondary dark:text-secondary-dark">Latest posts</h2>
+<Section name="Latest posts">
 	{#each posts as post}
 		<a sveltekit:prefetch href={`blog/${post.slug}`}>
 			<PostListItem {post} small={true} />
 		</a>
 	{/each}
-	<a class="flex mt-4 text-gray hover:text-secondary dark:hover:text-secondary-dark" href="blog"
-		>Read all posts<i>{@html arrowRightIcon}</i></a
+	<a
+		class="flex mt-4 text-gray dark:text-gray-3 hover:text-secondary dark:hover:text-secondary-dark"
+		href="blog">Read all posts<i>{@html arrowRightIcon}</i></a
 	>
-</section>
-
-<style lang="postcss">
-	.name {
-		@apply text-primary font-black font-hack;
-	}
-</style>
+</Section>
