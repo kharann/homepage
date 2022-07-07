9c2b649e42b32ef2c400655f365083db676e3ae0
kharann <dev@akvo.no>
2022-07-07 13:25:31 +0200

    add list-item component for portable text

---
+++ web/src/components/portable-text/portable-list-item.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { ListItemComponentProps } from '@portabletext/svelte'
+	export let portableText: ListItemComponentProps
+	$: ({ value } = portableText)
+	$: ({ listItem } = value)
+</script>
+
+{#if listItem === 'bullet'}
+	<li><slot /></li>
+{:else}
+	<li><slot /></li>
+{/if}
