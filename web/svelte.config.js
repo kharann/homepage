856fd201456466cd71a8328b471949b5834a2475
kharann <dev@akvo.no>
2022-07-07 14:01:48 +0200

    use netlify sveltekit adapter

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static'
-import path from 'path'
+import adapter from '@sveltejs/adapter-netlify'
 
 const dev = process.env.NODE_ENV === 'development'
 
