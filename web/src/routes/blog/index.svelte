78a20b3cc864329a769d152c887e156f47ce8ecc
kharann <dev@akvo.no>
2022-07-13 10:48:58 +0200

    update blog text

--- web/src/routes/blog/index.svelte
+++ web/src/routes/blog/index.svelte
@@ -9,13 +9,8 @@
 <section>
 	<h1 class="text-4xl font-bold text-primary-9">Blog</h1>
 	<p class="my-4">
-		In 2021, I decided to begin sharing my thoughts in a more personal manner. Originally this
-		website was supposed to be a knowledge base where I store things I've learned or snippets for
-		myself in the future. However, I changed my mind and decided to share my stories, whether it's
-		about using a new technology or a mistake I've learned from.
-	</p>
-	<p class="my-4">
-		Currently, I've posted {posts.length} post, and I mostly write about technology, process, and careers.
+		In 2021, I decided to start writing more on my own time. After all, I love teaching and sharing
+		my experiences. I mostly write about web development, cloud, team processes or tech careers.
 	</p>
 	<Section name="All posts.">
 		<LatestPosts {posts} />
