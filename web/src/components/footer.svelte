c69af76f0cc27497df6fa0538aff80ced5a98213
Anhkha Vo <anhkhav@gmail.com>
2022-07-15 21:33:48 +0000

    increase tap target of links in footer

--- web/src/components/footer.svelte
+++ web/src/components/footer.svelte
@@ -25,15 +25,14 @@
 	<div class="m-auto flex h-full w-full max-w-4xl flex-col items-center">
 		<div class="grid w-max grid-cols-3 gap-5">
 			{#each iconLinks as iconLink}
-				<a class="text-highlight duration-300 hover:text-primary-7" href={iconLink.link} aria-hidden={true}
-					>{@html iconLink.icon}<span class="sr-only">{iconLink.text}</span></a
+				<Link url={iconLink.link}
+					><i aria-hidden="true">{@html iconLink.icon}</i><span class="sr-only">{iconLink.text}</span></Link
 				>
 			{/each}
 		</div>
-		<div class="mb-4 mt-4 border-b-2 border-accent pb-2">
+		<div class="mb-6 mt-4">
 			<Link url="https://github.com/kharann/homepage" accent={false}
-				>Website built by Anh-Kha Vo</Link
-			>
+				><span class="py-2 px-1 border-b-2 border-accent">Website built by Anh-Kha Vo</span></Link>
 		</div>
 	</div>
 </footer>
