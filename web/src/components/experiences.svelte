3937d4e770dfc2cc4fb8ab22d73a1441764ca96a
kharann <dev@akvo.no>
2022-07-22 15:41:09 +0200

    work on transitions with svelte

--- web/src/components/experiences.svelte
+++ web/src/components/experiences.svelte
@@ -1,24 +1,37 @@
 <script lang="ts">
 	import type { Experience } from '@api/all-experience'
-	import { DateTime } from 'luxon'
-	import feather from 'feather-icons'
-	// const chevronIcon = feather.icons["chevron-down"].toSvg()
 
 	export let experiences: Experience[]
+	let active: number = 0
+
+	const handleClick = (tab: number) => {
+		active = tab
+	}
 </script>
 
-<ul class="mt-8">
-	{#each experiences as exp}
-		<li class="flex justify-between items-center sm:my-2 my-4">
-			<div class="flex-start flex flex-col text-gray-600 sm:flex-row">
+<div class="mt-8 flex">
+	<div class="relative h-10 w-thin bg-accent" animate:hmm={{ duration: 2000 }} />
+	<ul class="flex flex-col">
+		{#each experiences as exp, i}
+			<li
+				class={`p-2 flex justify-center ${
+					i == active ? 'bg-slate-100 text-foreground-accent font-medium' : 'bg-background'
+				}`}
+			>
+				<button class="" on:click={() => handleClick(i)}>
+					{exp.title}
+				</button>
+			</li>
+			<!-- <div class="flex-start flex flex-col text-gray-600 sm:flex-row">
 				<div class="w-24 sm:mr-20">
 					<time datetime={exp.start}>{DateTime.fromISO(exp.start).year}</time>
 					-
 					<time datetime={exp.end}>{exp.end ? DateTime.fromISO(exp.end).year : ' Now'}</time>
 				</div>
 				<span class="text-foreground">{exp.role} at {exp.title}</span>
-			</div>
+			</div> -->
 			<!-- <i>{@html  chevronIcon}</i> -->
-		</li>
-	{/each}
-</ul>
+		{/each}
+	</ul>
+	<div class="">content</div>
+</div>
