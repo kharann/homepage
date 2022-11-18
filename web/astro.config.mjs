ee74d033f64737b68a86a6e00e531eeeebd48cd3
Anhkha Vo <anhkhav@gmail.com>
2022-11-18 23:37:23 +0000

    add party town

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,19 +1,18 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
-import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-import image from '@astrojs/image'
+import image from '@astrojs/image';
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		svelte(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp'
-		})
-	]
-})
+  integrations: [tailwind(), svelte(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), partytown()]
+});
