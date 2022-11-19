2ce07ede6668d0531e4d2ec14eba8c2bb7b3d474
kharann <anhkhav@gmail.com>
2022-11-19 04:12:54 +0100

    test out panelbear

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
-import image from '@astrojs/image';
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), partytown()]
-});
+	integrations: [
+		tailwind(),
+		svelte(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp'
+		}),
+		partytown()
+	]
+})
