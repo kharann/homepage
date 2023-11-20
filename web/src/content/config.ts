a288bf09df81febc34496c46783d4c124e00c11c
Kharann <hey@akvo.no>
2023-11-21 00:55:52 +0100

    fix building

--- web/src/content/config.ts
+++ web/src/content/config.ts
@@ -1,15 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()),
+	draft: z.boolean().optional().default(false)
+  }),
 });
 
 export const collections = { blog };
---
+++ web/src/types.ts
@@ -0,0 +1,7 @@
+export type Frontmatter = {
+  title: string;
+  pubDate: string;
+  description: string;
+  tags: string[];
+  minutesRead: number;
+};
